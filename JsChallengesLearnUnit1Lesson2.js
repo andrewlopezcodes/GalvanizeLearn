490754020041554a5ffb071798e8cba1c4792f3a
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-05-02 19:30:07 -0400

    initial commit

---
+++ JsChallengesLearnUnit1Lesson2.js
@@ -0,0 +1,4 @@
+/* Go to the above JSFiddle and change the variable yourName to, well, your name!.
+Make sure you are logged in! Once you've finished, hit "Save" and paste the new URL below. */
+
+https://jsfiddle.net/mrandrewlopez/y5vzoesw/
---
+++ JsChallengesUnit1Lesson1.js
@@ -0,0 +1,6 @@
+/* Change the below function to return true instead of false. */
+
+function isExcitedToStartCoding () {
+  return true;
+}
+
